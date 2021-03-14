3b06d4bc1bca03350dd22cbba2683c7cbdc56e3c
Julian Thijssen <Nimthora@gmail.com>
2021-03-14 17:10:29 +0100

    Update thesia to have more granularity and add smoothing

--- MMIGroup1Project/Assets/Scripts/AudioSpectrumData.cs
+++ MMIGroup1Project/Assets/Scripts/AudioSpectrumData.cs
@@ -6,30 +6,31 @@ using UnityEngine;
 public class AudioSpectrumData : MonoBehaviour
 {
     private GameObject thesiaObj;
-    private LineRenderer lineRenderer;
     private Material material;
 
-    private List<Vector3> pos = new List<Vector3>();
+    //private List<Vector3> pos = new List<Vector3>();
+    private const int SAMPLES = 256;
+    private float[] freqAmps = new float[SAMPLES * 4];
+    private int avgIndex = 0;
 
     void Start()
     {
         thesiaObj = GameObject.Find("Thesia");
-        lineRenderer = thesiaObj.GetComponent<LineRenderer>();
         material = thesiaObj.GetComponent<Renderer>().material;
     }
 
     void Update()
     {
-        float[] spectrum = new float[512];
+        float[] spectrum = new float[SAMPLES * 8];
 
         AudioListener.GetSpectrumData(spectrum, 0, FFTWindow.Rectangular);
 
-        float freqResolution = 24000.0f / 512;
+        float freqResolution = 24000.0f / (SAMPLES * 8);
 
         float maxAmp = 0;
 
-        pos.Clear();
-        for (int i = 0; i < spectrum.Length / 8; i++)
+        //pos.Clear();
+        for (int i = 0; i < SAMPLES; i++)
         {
             float freq = i * freqResolution;
             float freqAmp = spectrum[i];
@@ -37,8 +38,11 @@ public class AudioSpectrumData : MonoBehaviour
 
             //pos.Add(new Vector3(i / 64.0f - 1, -1, 0));
             //pos.Add(new Vector3(i / 64.0f - 1, -1 + freqAmp * 5, 0));
-            pos.Add(new Vector3(0, 0.6f + freqAmp * 3, 0));
+            //pos.Add(new Vector3(0, 0.6f + freqAmp * 3, 0));
+            freqAmps[avgIndex * SAMPLES + i] = freqAmp;
         }
+        avgIndex++;
+        if (avgIndex == 4) avgIndex = 0;
 
         //lineRenderer.startWidth = 0.02f;
         //lineRenderer.endWidth = 0.02f;
@@ -62,18 +66,26 @@ public class AudioSpectrumData : MonoBehaviour
         
         GL.Color(darkGreen);
         float rot = 0;
-        foreach (Vector3 p in pos)
+        for (int i = 0; i < SAMPLES; i++)
         {
-            float x = p.y * Mathf.Cos(rot);
-            float y = p.y * Mathf.Sin(rot);
+            float amp = 0;
+            for (int a = 0; a < 4; a++)
+            {
+                amp += 0.6f + Mathf.Min(freqAmps[a * SAMPLES + i] * 3, 0.5f);// Mathf.Max(0, Mathf.Log(freqAmps[a * SAMPLES + i] * 3) + 1);
+            }
+            amp /= 4;
+            //amp = Mathf.Log(amp) + 1;
+
+            float x = amp * Mathf.Cos(rot);
+            float y = amp * Mathf.Sin(rot);
 
             GL.Vertex3(0.5f * Mathf.Cos(rot), 0.5f * Mathf.Sin(rot), 0);
             GL.Vertex3(x, y, 0);
 
-            rot -= (2 * Mathf.PI) / pos.ToArray().Length;
+            rot -= (2 * Mathf.PI) / SAMPLES;
 
-            x = p.y * Mathf.Cos(rot);
-            y = p.y * Mathf.Sin(rot);
+            x = amp * Mathf.Cos(rot);
+            y = amp * Mathf.Sin(rot);
             
             GL.Vertex3(x, y, 0);
             GL.Vertex3(0.5f * Mathf.Cos(rot), 0.5f * Mathf.Sin(rot), 0);
