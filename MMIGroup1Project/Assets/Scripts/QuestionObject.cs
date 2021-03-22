604dc96c3ad812b97d35de5d73aa529867b1668a
Nikola Grigorov <nikolagrigorov3@gmail.com>
2021-03-22 07:26:16 +0100

    Cleaned up the scene, removed useless stuff

--- MMIGroup1Project/Assets/Scripts/QuestionObject.cs
+++ MMIGroup1Project/Assets/Scripts/QuestionObject.cs
@@ -6,7 +6,6 @@ public class QuestionObject : MonoBehaviour
 {
     
     public TextMeshProUGUI textUI;
-    public List<AnswerObject> answers;
 
     public string question = "Who dis new fone?";
     public int correctAnswer = 0;
@@ -14,31 +13,5 @@ public class QuestionObject : MonoBehaviour
     void Start()
     {
         textUI.text = question;
-        for (int i = 1; i < 4; i++)
-        {
-            answers.Add(this.transform.GetChild(i).GetComponent<AnswerObject>());
-        }
-    }
-
-    public void questionAnswer(AnswerObject caller)
-    {
-        if(answers.IndexOf(caller) == correctAnswer)
-        {
-            //Correct answer
-            Debug.Log("Correct answer");
-        }
-        else
-        {
-            //incorrect answer
-            Debug.Log("Incorrect answer");
-        }
-    }
-
-    public void disableAnswers()
-    {
-        foreach (AnswerObject item in answers)
-        {
-            item.gameObject.GetComponent<BoxCollider>().enabled = false;
-        }
     }
 }
