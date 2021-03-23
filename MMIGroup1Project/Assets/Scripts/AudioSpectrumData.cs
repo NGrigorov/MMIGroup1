d1c7361a9a9e1aae72046098f76b665c7adb476c
Julian Thijssen <Nimthora@gmail.com>
2021-03-23 21:20:17 +0100

    Update Thesia to have more sophisticated visualization

--- MMIGroup1Project/Assets/Scripts/AudioSpectrumData.cs
+++ MMIGroup1Project/Assets/Scripts/AudioSpectrumData.cs
@@ -8,10 +8,13 @@ public class AudioSpectrumData : MonoBehaviour
     private GameObject thesiaObj;
     private Material material;
 
-    //private List<Vector3> pos = new List<Vector3>();
-    private const int SAMPLES = 256;
-    private float[] freqAmps = new float[SAMPLES * 4];
+    // Number of frequency samples
+    public const int SAMPLES = 64;
+
+    private const int smoothingFactor = 4;
+    private float[] freqAmps = new float[SAMPLES * smoothingFactor];
     private int avgIndex = 0;
+    private float maxAmp = 0;
 
     void Start()
     {
@@ -21,78 +24,125 @@ public class AudioSpectrumData : MonoBehaviour
 
     void Update()
     {
+        maxAmp = 0;
         float[] spectrum = new float[SAMPLES * 8];
 
         AudioListener.GetSpectrumData(spectrum, 0, FFTWindow.Rectangular);
 
         float freqResolution = 24000.0f / (SAMPLES * 8);
 
-        float maxAmp = 0;
-
-        //pos.Clear();
         for (int i = 0; i < SAMPLES; i++)
         {
             float freq = i * freqResolution;
             float freqAmp = spectrum[i];
             if (freqAmp > maxAmp) maxAmp = freqAmp;
 
-            //pos.Add(new Vector3(i / 64.0f - 1, -1, 0));
-            //pos.Add(new Vector3(i / 64.0f - 1, -1 + freqAmp * 5, 0));
-            //pos.Add(new Vector3(0, 0.6f + freqAmp * 3, 0));
             freqAmps[avgIndex * SAMPLES + i] = freqAmp;
         }
         avgIndex++;
-        if (avgIndex == 4) avgIndex = 0;
-
-        //lineRenderer.startWidth = 0.02f;
-        //lineRenderer.endWidth = 0.02f;
-        //lineRenderer.SetPositions(pos.ToArray());
-        //lineRenderer.useWorldSpace = true;
-        //lineRenderer.positionCount = pos.ToArray().Length;
-        //lineRenderer.loop = false;
-
-        //thesiaObj.transform.position = new Vector3(0, maxAmp * 10, 0);
+        if (avgIndex == smoothingFactor) avgIndex = 0;
     }
 
-    void OnPostRender()
+    private void RenderCircle(float x, float y, float radius, Color color)
+    {
+        GL.Begin(GL.TRIANGLES);
+        GL.Color(color);
+        for (int i = 0; i < 32; i++)
+        {
+            float rot1 = (i / 32.0f) * (Mathf.PI * 2);
+            float rot2 = ((i+1) / 32.0f) * (Mathf.PI * 2);
+            GL.Vertex3(x, y, 0);
+            GL.Vertex3(x + radius * Mathf.Cos(-rot1), y + radius * Mathf.Sin(-rot1), 0);
+            GL.Vertex3(x + radius * Mathf.Cos(-rot2), y + radius * Mathf.Sin(-rot2), 0);
+        }
+        GL.End();
+    }
+
+    private void RenderStraight(float x1, float x2, float y1, float y2)
+    {
+        GL.TexCoord2(0, 0);
+        GL.Vertex3(x1, y1, 0);
+        GL.TexCoord2(0, 1);
+        GL.Vertex3(x1, y2, 0);
+
+        GL.TexCoord2(1, 1);
+        GL.Vertex3(x2, y2, 0);
+        GL.TexCoord2(1, 0);
+        GL.Vertex3(x2, y1, 0);
+    }
+
+    private void RenderCurved(float x1, float x2, float y1, float y2, float amp, float range)
+    {
+        float offset = Mathf.PI * 0.5f + range * 0.5f;
+
+        x1 = -x1 * range + offset;
+        x2 = -x2 * range + offset;
+
+        float radius = 1.2f;
+
+        float lowRadius = Mathf.Max(radius - amp, 0.5f);
+
+        GL.TexCoord2(0, 0);
+        GL.Vertex3(lowRadius * Mathf.Cos(x1), lowRadius * Mathf.Sin(x1), 0);
+        GL.TexCoord2(0, 1);
+        GL.Vertex3((radius + amp) * Mathf.Cos(x1), (radius + amp) * Mathf.Sin(x1), 0);
+
+        GL.TexCoord2(1, 1);
+        GL.Vertex3((radius + amp) * Mathf.Cos(x2), (radius + amp) * Mathf.Sin(x2), 0);
+        GL.TexCoord2(1, 0);
+        GL.Vertex3(lowRadius * Mathf.Cos(x2), lowRadius * Mathf.Sin(x2), 0);
+    }
+
+    private void RenderFrequency()
     {
         Color darkGreen = new Color(0, 0.5f, 0, 1);
 
         GL.PushMatrix();
         material.SetPass(0);
         GL.LoadIdentity();
+        float aspect = (float) Screen.width / Screen.height;
+        var proj = Matrix4x4.Ortho(0, 10, 0, 10, -1, 100);
+        GL.LoadProjectionMatrix(proj);
+
+        GL.PushMatrix();
+        GL.LoadIdentity();
+        var model = Matrix4x4.Translate(new Vector3(3, 3, 0));
+        GL.MultMatrix(model);
+
+        RenderCircle(0, 0, 2, new Color(34 / 255.0f, 34 / 255.0f, 59 / 255.0f));
+        RenderCircle(0, 0, maxAmp, new Color(72 / 255.0f, 191 / 255.0f, 227 / 255.0f));
 
         GL.Begin(GL.QUADS);
-        
-        GL.Color(darkGreen);
-        float rot = 0;
+
+        float div = SAMPLES;
+        float offset = 0.2f / SAMPLES;
+
         for (int i = 0; i < SAMPLES; i++)
         {
+            // Compute moving average amplitude of frequency samples
             float amp = 0;
-            for (int a = 0; a < 4; a++)
+            for (int a = 0; a < smoothingFactor; a++)
             {
-                amp += 0.6f + Mathf.Min(freqAmps[a * SAMPLES + i] * 3, 0.5f);// Mathf.Max(0, Mathf.Log(freqAmps[a * SAMPLES + i] * 3) + 1);
+                amp += Mathf.Min(freqAmps[a * SAMPLES + i] * 10 + 0.1f, 1.0f);// Mathf.Max(0, Mathf.Log(freqAmps[a * SAMPLES + i] * 3) + 1);
             }
-            amp /= 4;
-            //amp = Mathf.Log(amp) + 1;
+            amp /= smoothingFactor;
 
-            float x = amp * Mathf.Cos(rot);
-            float y = amp * Mathf.Sin(rot);
+            float x1 = (i / div) + offset;
+            float x2 = ((i + 1) / div) - offset;
+            float y1 = -amp;
+            float y2 = amp;
 
-            GL.Vertex3(0.5f * Mathf.Cos(rot), 0.5f * Mathf.Sin(rot), 0);
-            GL.Vertex3(x, y, 0);
-
-            rot -= (2 * Mathf.PI) / SAMPLES;
-
-            x = amp * Mathf.Cos(rot);
-            y = amp * Mathf.Sin(rot);
-            
-            GL.Vertex3(x, y, 0);
-            GL.Vertex3(0.5f * Mathf.Cos(rot), 0.5f * Mathf.Sin(rot), 0);
+            //RenderStraight(x1, x2, y1, y2);
+            RenderCurved(x1, x2, y1, y2, amp, 4);
         }
 
         GL.End();
-
         GL.PopMatrix();
+        GL.PopMatrix();
+    }
+
+    void OnPostRender()
+    {
+        RenderFrequency();
     }
 }
